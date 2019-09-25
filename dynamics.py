4e3b9e9b229021acdbfdb4ab70670f8dd88e86f1
hocherie <ho.cherie@gmail.com>
2019-09-25 16:43:32 -0400

    added goToAccerlation to controller.py

--- dynamics.py
+++ dynamics.py
@@ -233,7 +233,7 @@ class QuadDynamics:
                 [1, 0, -np.sin(theta[1])],
                 [0, np.cos(theta[0]), np.cos(theta[1])*np.sin(theta[0])],
                 [0, -np.sin(theta[0]), np.cos(theta[1])*np.cos(theta[0])]
-            ], dtype='float')
+            ], dtype='double')
 
         mult_inv = np.linalg.inv(mult_matrix)
         thetadot = np.dot(mult_inv, omega)
