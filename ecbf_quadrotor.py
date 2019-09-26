ade4825a29b5e6ee3afe2c3a8dad5a82758b8a96
hocherie <ho.cherie@gmail.com>
2019-09-25 22:44:33 -0400

    changed dt= 10e-3, overdamped

--- ecbf_quadrotor.py
+++ ecbf_quadrotor.py
@@ -15,7 +15,7 @@ class SimpleDynamics():
         rd = np.array([np.array([0, 0])]).T  # velocity
         self.state = {"r":r, "rd":rd}
         ## Params
-        self.dt = 10e-2
+        self.dt = 10e-3
 
         # self.u = zeros(2,1) # acceleration, control input
 
@@ -167,6 +167,7 @@ def main():
         state = dyn.step_dynamics(state, u_motor)
         ecbf.state = state
         state_hist.append(state["x"])
+<<<<<<< Updated upstream
         if(tt % 100 == 0):
             print(tt)
             plt.cla()
@@ -185,6 +186,26 @@ def main():
 
             ecbf.plot_h(new_obs)
 
+=======
+        # if(tt % 100 == 0):
+        #     print(tt)
+        #     plt.cla()
+        #     state_hist_plot = np.array(state_hist)
+        #     nom_cont = ecbf.compute_nom_control()
+        #     plt.plot([state_hist_plot[-1, 0], state_hist_plot[-1, 0] + 100 *
+        #               u_hat_acc[0]],
+        #              [state_hist_plot[-1, 1], state_hist_plot[-1, 1] + 100 * u_hat_acc[1]], label="Safe")
+        #     plt.plot([state_hist_plot[-1, 0], state_hist_plot[-1, 0] + 100 *
+        #               nom_cont[0]], 
+        #              [state_hist_plot[-1, 1], state_hist_plot[-1, 1] + 100 * nom_cont[1]],label="Nominal")
+
+        #     plt.plot(state_hist_plot[:, 0], state_hist_plot[:, 1],'k')
+        #     plt.plot(ecbf.goal[0], ecbf.goal[1], '*r')
+        #     plt.plot(state_hist_plot[-1, 0], state_hist_plot[-1, 1], '*k') # current
+            
+        #     ecbf.plot_h(new_obs)
+    
+>>>>>>> Stashed changes
 
 
 
