4f4f44ed10642a59ad9d9e54778cc0d36841489e
hocherie <ho.cherie@gmail.com>
2019-09-27 11:09:29 -0400

    dump

---
+++ 3dquad_env/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='3dquad_env',
+      version='0.1',
+      description='2D Grid Environment with common utils (raytracing) and 3D quadrotor dynamics',
+      url='https://github.com/hocherie/2d_grid_playground',
+      author='Cherie Ho',
+      author_email='cherieh@cs.cmu.edu',
+      license='MIT',
+      packages=['3dquad_env'],
+      zip_safe=False)
