ce98cb33c9fab22d3d07ba81f5b89c875567124a
cherie <ho.cherie@gmail.com>
2019-10-31 11:46:49 -0400

    seems to work for two sets

--- ecbf_quadrotor.py
+++ ecbf_quadrotor.py
@@ -10,7 +10,7 @@ b = 1
 safety_dist = 1 # TODO: change
 
 class ECBF_control():
-    def __init__(self, state, goal=np.array([[0], [10]])):
+    def __init__(self, state, goal=np.array([[10], [0]])):
         self.state = state
         self.shape_dict = {}
         Kp = 4
@@ -30,8 +30,9 @@ class ECBF_control():
     def compute_h_hd(self):
         h = self.compute_h()
         hd = self.compute_hd()
-
-        return np.vstack((h, hd)).astype(np.double)
+        # print("h shape", h.shape)
+        # print("hd shape", hd.shape)
+        return np.hstack((h, hd)).astype(np.double)
 
     def compute_h(self, obs=None):
         # return self.compute_h_superellip(obs)
@@ -52,7 +53,7 @@ class ECBF_control():
     def compute_safe_control(self,obs):
         if self.use_safe:
             A = self.compute_A(obs)
-            assert(A.shape == (1,2))
+            assert(A.shape == (2,2))
 
             b_ineq = self.compute_b(obs)
 
@@ -68,7 +69,7 @@ class ECBF_control():
 
             optimized_u = sol['x']
             np.random.seed()
-            optimized_u += np.random.random()*np.linalg.norm(optimized_u)*.1
+            # optimized_u += np.random.random()*np.linalg.norm(optimized_u)
 
         else:
             optimized_u = self.compute_nom_control()
@@ -83,47 +84,58 @@ class ECBF_control():
 
         if np.linalg.norm(u_nom) > 1:
             u_nom = (u_nom/np.linalg.norm(u_nom))
+
+        u_nom = np.array([0.2, 0])
         return u_nom.astype(np.double)
 
     # Box-specific functions
     def compute_h_box(self):
         hr = h_func(self.state["x"][0], self.state["x"][1], a, b, safety_dist)
+        assert(hr.shape==(2,1))
         return hr
     
     def compute_hd_box(self):
-        hd = -self.state["xdot"][1]
+        hd1 = -self.state["xdot"][1]
+        hd2 = -self.state["xdot"][0]
+
+        hd = np.vstack((hd1, hd2))
+        assert(hd.shape==(2,1))
         return hd
 
     def compute_A_box(self):
-        A = np.array([[0], [-1]]).T
+        A = np.array([[0, -1], [-1, 0]])
+        assert(A.shape==(2,2))
+        # A2 = np.array()
         return A
 
     def compute_b_box(self):
-        # print
-        b_ineq = -self.K @ self.compute_h_hd()
-
+        # print("K", self.K.shape)
+        # print("hhd", self.compute_h_hd().shape)
+        b_ineq = - self.compute_h_hd() @ np.atleast_2d(self.K).T
+        # print(b_ineq.shape)
+        assert(b_ineq.shape==(2,1))
         return b_ineq     
 
-    # Superellipsoid-specific functions
-    def compute_h_superellip(self, obs):
-        rel_r = np.atleast_2d(self.state["x"][:2]).T - obs
-        # TODO: a, safety_dist, obs, b
-        hr = h_func(rel_r[0], rel_r[1], a, b, safety_dist)
-        return hr
+    # # Superellipsoid-specific functions
+    # def compute_h_superellip(self, obs):
+    #     rel_r = np.atleast_2d(self.state["x"][:2]).T - obs
+    #     # TODO: a, safety_dist, obs, b
+    #     hr = h_func(rel_r[0], rel_r[1], a, b, safety_dist)
+    #     return hr
 
-    def compute_hd_superellip(self, obs):
-        rel_r = np.atleast_2d(self.state["x"][:2]).T - obs
-        rd = np.atleast_2d(self.state["xdot"][:2]).T
-        term1 = (4 * np.power(rel_r[0],3) * rd[0])/(np.power(a,4))
-        term2 = (4 * np.power(rel_r[1],3) * rd[1])/(np.power(b,4))
-        return term1+term2
+    # def compute_hd_superellip(self, obs):
+    #     rel_r = np.atleast_2d(self.state["x"][:2]).T - obs
+    #     rd = np.atleast_2d(self.state["xdot"][:2]).T
+    #     term1 = (4 * np.power(rel_r[0],3) * rd[0])/(np.power(a,4))
+    #     term2 = (4 * np.power(rel_r[1],3) * rd[1])/(np.power(b,4))
+    #     return term1+term2
 
-    def compute_A_superellip(self, obs):
-        rel_r = np.atleast_2d(self.state["x"][:2]).T - obs
-        A0 = (4 * np.power(rel_r[0], 3))/(np.power(a, 4))
-        A1 = (4 * np.power(rel_r[1], 3))/(np.power(b, 4))
+    # def compute_A_superellip(self, obs):
+    #     rel_r = np.atleast_2d(self.state["x"][:2]).T - obs
+    #     A0 = (4 * np.power(rel_r[0], 3))/(np.power(a, 4))
+    #     A1 = (4 * np.power(rel_r[1], 3))/(np.power(b, 4))
 
-        return np.array([np.hstack((A0, A1))])
+    #     return np.array([np.hstack((A0, A1))])
 
 
     def compute_b_ellip(self, obs):
@@ -147,24 +159,34 @@ def h_func_superellip(r1, r2, a, b, safety_dist):
     return hr
 
 @np.vectorize
-def h_func_box(r1, r2, a, b, safety_dist):
-    #! start with one line (positive y)
+def h_func_box1(r1, r2, a, b, safety_dist):
+    #! (positive y)
     r_max = 5
-    laser_angle = np.radians(0) 
-    hr = (r_max - r2) - safety_dist # bound on max y
-    return hr
-    # pass
+    hr1 = (r_max - r2) - safety_dist # bound on max y
+    return hr1
 
+@np.vectorize
+def h_func_box2(r1, r2, a, b, safety_dist):
+    #! (positive x)
+    r_max = 5
+    hr2 = (r_max - r1) - safety_dist # bound on max y
+    return hr2
+
+def h_func_box(r1, r2, a, b, safety_dist):
+    # return (#set, 1) matrix
+    h = np.vstack((h_func_box1(r1, r2, a, b, safety_dist), h_func_box2(r1, r2, a, b, safety_dist)))
+    # print(h.shape)
+    return h
 
 def h_func(r1, r2, a, b, safety_dist):
     return h_func_box(r1, r2, a, b, safety_dist)
 
-def plot_h(obs, h_func=h_func_box):
+def plot_h(obs, h_func=h_func_box2):
 
     plot_x = np.arange(-10, 10, 0.1)
     plot_y = np.arange(-10, 10, 0.1)
     xx, yy = np.meshgrid(plot_x, plot_y, sparse=True)
-    z = h_func(xx - obs[0], yy - obs[1], a, b, safety_dist) > 0
+    z = h_func(xx, yy, a, b, safety_dist) > 0
     h = plt.contourf(plot_x, plot_y, z, [-1, 0, 1], colors=["black","white"])
     plt.xlabel("X")
     plt.ylabel("Y")
@@ -190,16 +212,17 @@ def run_trial(state, obs_loc,goal, num_it, variance):
         u_motor = go_to_acceleration(
             state, u_hat_acc, dyn.param_dict)  # desired motor rate ^2
         state = dyn.step_dynamics(state, u_motor)
-        noise_x += (np.random.rand(3) - 0.5) * variance # add gaussian to random walk
-        state["x"] += noise_x  # add noise to position position
+        # noise_x += (np.random.rand(3) - 0.5) * variance # add gaussian to random walk
+        # state["x"] += noise_x  # add noise to position position
         ecbf.state = state
         # ecbf.add_state_noise(variance) # Add noise here
-        state_hist.append(state["x"]-noise_x) # append true state
-        h_hist[tt] = ecbf.compute_h(new_obs)
+        state_hist.append(state["x"]) # append true state
+        # h_hist[tt] = ecbf.compute_h(new_obs)
         if(tt % 100 == 0):
             print(tt)
             plt.cla()
             state_hist_plot = np.array(state_hist)
+            # print("state_hist_plot size", state_hist_plot.shape)
             nom_cont = ecbf.compute_nom_control()
             plt.plot([state_hist_plot[-1, 0], state_hist_plot[-1, 0] + 100 *
                       u_hat_acc[0]],
