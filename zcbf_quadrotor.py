da0844b898dd1577a0250941b5104ae51f565b77
mmousaei <m.mousaei@gmail.com>
2020-06-03 18:27:02 -0400

    multirobot initial

--- zcbf_quadrotor.py
+++ zcbf_quadrotor.py
@@ -37,6 +37,8 @@ class ECBF_control():
         self.K = np.array([Kp, Kd])
         self.goal=goal
         self.use_safe = True
+        self.ecbf_safety_dist = 2.0
+
         # pass
 
     def compute_plot_z(self, obs):
@@ -63,57 +65,78 @@ class ECBF_control():
 
 
 
-    def compute_h(self, obs=np.array([[0], [0]]).T):
-        rel_r = np.atleast_2d(self.state["x"][:2]).T - obs
-        # TODO: a, safety_dist, obs, b
-        hr = h_func(rel_r[0], rel_r[1], a, b, safety_dist)
-        return hr
+    def compute_A(self,obs):
+        # print("pos: ", self.pos)
+        # print("col pos: ", self.col_pos)
+        del_p = np.atleast_2d(self.state["x"][:2]).T - obs
+        A1 = np.hstack((-del_p.T, del_p.T))
+        # A2 = np.hstack((del_p, -del_p))
+        # A = np.vstack((A1, A2))
+        # A = -del_p
+        print(A1.shape[1])
+        A1 = A1.reshape(1, A1.shape[1])
+        return A1
 
-    def compute_hd(self, obs):
-        rel_r = np.atleast_2d(self.state["x"][:2]).T - obs
-        rd = np.atleast_2d(self.state["xdot"][:2]).T
-        term1 = (4 * np.power(rel_r[0],3) * rd[0])/(np.power(a,4))
-        term2 = (4 * np.power(rel_r[1],3) * rd[1])/(np.power(b,4))
-        return term1+term2
+    def compute_b(self, obs, obs_v):
+        gamma = 0.005
+        alp1 = 1
+        alp2 = 1
+        
+        h = self.compute_h(obs, obs_v, 1)
+        del_pp = np.atleast_2d(self.state["x"][:2]).T - obs
+        del_vp = np.atleast_2d(self.state["xdot"][:2]).T - obs_v
+        b1 = gamma * h**3 * np.linalg.norm(del_pp) 
+        # b1 = self.gamma * np.exp(h) * np.linalg.norm(del_pp) 
+        b2 = (np.matmul(np.transpose(del_vp), del_pp))**2 / ((np.linalg.norm(del_pp))**2) 
+        alp12 = (alp1 + alp2)
+        b3 = alp12*np.matmul(np.transpose(del_vp), del_pp) / np.sqrt(2*alp12*(np.linalg.norm(del_pp) - self.ecbf_safety_dist))
+        b4 = np.linalg.norm(del_vp)**2
+        b11 = b1 - b2 + b3 + b4
+        h = self.compute_h(obs, obs_v, 2)
+        del_pm = -np.atleast_2d(self.state["x"][:2]).T + obs
+        del_vm = -np.atleast_2d(self.state["xdot"][:2]).T + obs_v
+        b1 = gamma * h**3 * np.linalg.norm(del_pm) 
+        # b1 = self.gamma * np.exp(h) * np.linalg.norm(del_pm) 
+        b2 = (np.matmul(np.transpose(del_vm), del_pm))**2 / ((np.linalg.norm(del_pm))**2) 
+        alp12 = (alp1 + alp2)
+        b3 = alp12*np.matmul(np.transpose(del_vm), del_pm) / np.sqrt(2*alp12*(np.linalg.norm(del_pm) - self.ecbf_safety_dist))
+        b4 = np.linalg.norm(del_vm)**2
+        b = np.vstack((b11, b1 - b2 + b3 + b4))
+        return b11
+    def compute_h(self, obs, obs_v, i):
+        alp1 = 1
+        alp2 = 1
+        if i == 1:
+            del_p = np.atleast_2d(self.state["x"][:2]).T - obs
+            del_v = np.atleast_2d(self.state["xdot"][:2]).T - obs_v
 
-    def compute_A(self, obs):
-        rel_r = np.atleast_2d(self.state["x"][:2]).T - obs
-        A0 = (4 * np.power(rel_r[0], 3))/(np.power(a, 4))
-        A1 = (4 * np.power(rel_r[1], 3))/(np.power(b, 4))
+            h1 = np.sqrt(2*(alp1+alp2)*(np.linalg.norm(del_p) - self.ecbf_safety_dist))
+            h2 = np.matmul(np.transpose(del_p), del_v)/np.linalg.norm(del_p)
+        elif i == 2:
+            del_p = obs - np.atleast_2d(self.state["x"][:2]).T
+            del_v = obs_v - np.atleast_2d(self.state["xdot"][:2]).T
 
-        return np.array([np.hstack((A0, A1))])
+            h1 = np.sqrt(2*(alp1+alp2)*(np.linalg.norm(del_p) - self.ecbf_safety_dist))
+            h2 = np.matmul(np.transpose(del_p), del_v)/np.linalg.norm(del_p)
 
-    def compute_h_hd(self, obs):
-        h = self.compute_h(obs)
-        hd = self.compute_hd(obs)
+        # print("in  = "+str(2*(self.alp1+self.alp2)*(np.linalg.norm(del_p) - self.ecbf_safety_dist)))
+        # print("h1  = "+str(h1))
+        # print("h2 = "+str(h2))
+        return h1+h2
 
-        return np.vstack((h, hd)).astype(np.double)
-
-    def compute_b(self, obs):
-        """extra + K * [h hd]"""
-        rel_r = np.atleast_2d(self.state["x"][:2]).T - obs
-        rd = np.array(np.array(self.state["xdot"])[:2])
-        extra = -(
-            (12 * np.square(rel_r[0]) * np.square(rd[0]))/np.power(a,4) +
-            (12 * np.square(rel_r[1]) * np.square(rd[1]))/np.power(b, 4)
-        )
-
-        b_ineq = extra - self.K @ self.compute_h_hd(obs)
-        return b_ineq
-
-    def compute_safe_control(self,obs):
+    def compute_safe_control(self, obs, obs_v):
         if self.use_safe:
             A = self.compute_A(obs)
-            assert(A.shape == (1,2))
+            assert(A.shape == (1,4))
 
-            b_ineq = self.compute_b(obs)
+            b_ineq = self.compute_b(obs, obs_v)
 
             #Make CVXOPT quadratic programming problem
             P = matrix(np.eye(2), tc='d')
             q = -1 * matrix(self.compute_nom_control(), tc='d')
-            G = -1 * matrix(A.astype(np.double), tc='d')
+            G = matrix(A.astype(np.double), tc='d')
 
-            h = -1 * matrix(b_ineq.astype(np.double), tc='d')
+            h = matrix(b_ineq.astype(np.double), tc='d')
             solvers.options['show_progress'] = False
             sol = solvers.qp(P,q,G, h, verbose=False) # get dictionary for solution
 
@@ -146,8 +169,8 @@ def h_func(r1, r2, a, b, safety_dist):
     return hr
 
 
-def robot_step(state, state_hist, dyn, ecbf, new_obs):
-    u_hat_acc = ecbf.compute_safe_control(obs=new_obs)
+def robot_step(state, state_hist, dyn, ecbf, new_obs, obs_v):
+    u_hat_acc = ecbf.compute_safe_control(obs=new_obs, obs_v=obs_v)
     u_hat_acc = np.ndarray.flatten(np.array(np.vstack((u_hat_acc,np.zeros((1,1))))))  # acceleration
     assert(u_hat_acc.shape == (3,))
     u_motor = go_to_acceleration(state, u_hat_acc, dyn.param_dict) # desired motor rate ^2
@@ -211,9 +234,11 @@ def main():
     for tt in range(20000):
         new_obs1 = state2["x"][:2].reshape(2,1)
         new_obs2 = state1["x"][:2].reshape(2,1)
+        obs_v1 = state2["xdot"][:2].reshape(2,1)
+        obs_v2 = state1["xdot"][:2].reshape(2,1)
         # print(new_obs1.shape)
-        u_hat_acc1 = robot_step(state1, state1_hist, dyn, ecbf1, new_obs1)
-        u_hat_acc2 = robot_step(state2, state2_hist, dyn, ecbf2, new_obs2)
+        u_hat_acc1 = robot_step(state1, state1_hist, dyn, ecbf1, new_obs1, obs_v1)
+        u_hat_acc2 = robot_step(state2, state2_hist, dyn, ecbf2, new_obs2, obs_v2)
 
         if(tt % 10 == 0):
             print(tt)
